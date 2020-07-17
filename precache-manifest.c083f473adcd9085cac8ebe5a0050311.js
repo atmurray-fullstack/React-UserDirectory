61724661ba853c086d4bc14736af1ac0e913fadc
Andrew Murray <atmurray@bellsouth.net>
2020-07-17 10:01:37 -0400

    Updates

---
+++ precache-manifest.c083f473adcd9085cac8ebe5a0050311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4a5a656cdcb17485b3a326222d3600",
+    "url": "/React-UserDirectory/index.html"
+  },
+  {
+    "revision": "65d9f6863512455690de",
+    "url": "/React-UserDirectory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "510bbeb8316091b82478",
+    "url": "/React-UserDirectory/static/js/2.7c9771c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-UserDirectory/static/js/2.7c9771c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d9f6863512455690de",
+    "url": "/React-UserDirectory/static/js/main.eef9681f.chunk.js"
+  },
+  {
+    "revision": "c8ffa9c1eff5c8cb9716",
+    "url": "/React-UserDirectory/static/js/runtime-main.cac8263e.js"
+  }
+]);
